bcfa084f15147f9844784b15de386608cd5e7a1e
Funder <adityapradipta994@gmail.com>
2019-12-20 09:19:25 +0800

    sec

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import User from "@/components/User"
+import Home from "@/components/Home";
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: "/user/:id",
+      name: "User",
+      component: User
+    }
+  ]
+})
